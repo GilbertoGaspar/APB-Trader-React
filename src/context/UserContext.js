972de65957a81dc14dbac014c4845d5a8b1c6027
Gilberto Gaspar <gilbertogaspar1996@gmail.com>
2021-06-19 02:57:00 -0400

    feat: update UserContext to use firebaseAuth and Provider's value structure

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,12 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { firebaseAuth } from '../firebase/firebase';
 
 const UserContext = React.createContext(null);
 
 const UserProvider = ({ children }) => {
   const [user, setUser] = useState(null);
 
+  useEffect(() => {
+    const unsubscribe = firebaseAuth.onAuthStateChanged((newUser) => {
+      if (newUser) {
+        setUser(newUser);
+      } else {
+        setUser(null);
+      }
+    });
+
+    return () => unsubscribe();
+  }, [user]);
+
   return (
-    <UserContext.Provider value={{ user, setUser }}>
+    <UserContext.Provider value={[user, setUser]}>
       {children}
     </UserContext.Provider>
   );
