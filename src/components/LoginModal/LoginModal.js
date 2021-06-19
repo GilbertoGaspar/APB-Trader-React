f633bd4fd1e68066edf6afec4c8a2c4fea8d29cb
Gilberto Gaspar <gilbertogaspar1996@gmail.com>
2021-06-19 02:59:03 -0400

    feat: add functionality to LoginModal

--- src/components/LoginModal/LoginModal.js
+++ src/components/LoginModal/LoginModal.js
@@ -1,85 +1,86 @@
-import React, {Component} from 'react'
+import React, { useState, useContext } from 'react';
+import { FiMail, FiLock } from 'react-icons/fi';
 
-import classes from './LoginModal.module.scss'
+import { firebaseAuth } from '../../firebase/firebase';
+import UserContext from '../../context/UserContext';
 
-export default class LoginModal extends Component {
+import Modal from '../Modal/Modal';
+import Button from '../Button/Button';
 
-    state = {
-        email : '',
-        pass : ''
-    }
+import classes from './LoginModal.module.scss';
 
-    render() {
+export default function LoginModal({ closeModalCallback }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMsg, setErrorMsg] = useState('');
+  const [, setUser] = useContext(UserContext);
 
-        //Displays default modal to login.
-        let currentModal = (
-            <div className={`modal fade ${classes['text-black']}`} id="loginModalCenter" tabIndex="-1" role="dialog" aria-labelledby="loginModalCenterTitle" aria-hidden="true">
-                <div className="modal-dialog modal-dialog-centered" role="document">
-                    <div className="modal-content">
-                    <div className="modal-header">
-                        <h5 className="modal-title" id="loginModalLongTitle">Log In</h5>
-                        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div className="modal-body">
-                        <div className="input-group mb-3">
-                            <div className="input-group-prepend">
-                                <span className="input-group-text" id="basic-addon1">Email</span>
-                            </div>
-                            <input type="text" className="form-control" placeholder="Email" aria-label="Email" aria-describedby="basic-addon1"
-                                onChange={(event) => this.setState({ email : event.target.value })}/>
-                        </div>
-                        <div className="input-group mb-3">
-                            <div className="input-group-prepend">
-                                <span className="input-group-text" id="basic-addon1">Password</span>
-                            </div>
-                            <input type="password" className="form-control" placeholder="Password" aria-label="Password" aria-describedby="basic-addon1"
-                                onChange={(event) => this.setState({ pass : event.target.value })}/>
-                        </div>
-                    </div>
-                    <div className="modal-footer">
-                        <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-                        <button type="button" className="btn btn-outline-success" 
-                            onClick={() => this.props.register(this.state.email, this.state.pass)}>Register</button>
-                        <button type="button" className="btn btn-success" 
-                            onClick={() => this.props.login(this.state.email, this.state.pass)}>Login</button>
-                    </div>
-                    </div>
-                </div>
-            </div>
-        )
-        
-        //If there is a current user show them the logout modal.
-        if(this.props.currentUser) {
-            currentModal = (
-                    <div className={`modal fade ${classes['text-black']}`} id="loginModalCenter" tabIndex="-1" role="dialog" aria-labelledby="loginModalCenterTitle" aria-hidden="true">
-                        <div className="modal-dialog modal-dialog-centered" role="document">
-                            <div className="modal-content">
-                            <div className="modal-header">
-                                <h5 className="modal-title" id="loginModalLongTitle">Log Out?</h5>
-                                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>
-                            <div className="modal-body">
-                                <p>Would you like to logout?</p>
-                            </div>
-                            <div className="modal-footer">
-                                <button type="button" className="btn btn-secondary" data-dismiss="modal">No</button>
-                                <button type="button" className="btn btn-success" 
-                                    onClick={() => this.props.logout()}>Yes</button>
-                            </div>
-                            </div>
-                        </div>
-                    </div>
-            )
-        }
+  const handleEmailChange = (e) => setEmail(e.target.value);
+  const handlePasswordChange = (e) => setPassword(e.target.value);
 
-        return (
-            <React.Fragment>
-                {currentModal}
-            </React.Fragment>
-        )
-    }
+  const handleCloseModal = () => {
+    setEmail('');
+    setPassword('');
+    setErrorMsg('');
+    closeModalCallback();
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    firebaseAuth
+      .signInWithEmailAndPassword(email, password)
+      .then(({ user }) => {
+        setUser(user);
+        handleCloseModal();
+      })
+      .catch(() => {
+        setErrorMsg('Username and/or Password are invalid.');
+        setPassword('');
+      });
+  };
+
+  return (
+    <Modal closeModalCallback={handleCloseModal}>
+      <h2 className={classes['modal__title']}>Login now!</h2>
+      <hr />
+      <form
+        id='login-form'
+        className={classes['modal__content']}
+        onSubmit={handleSubmit}
+      >
+        <label className={classes['modal__inputgroup']} htmlFor='email'>
+          <FiMail size='2rem' />
+          <input
+            className={classes['modal__input']}
+            name='email'
+            type='email'
+            required
+            value={email}
+            onChange={handleEmailChange}
+            placeholder='Email'
+          />
+        </label>
+        <label className={classes['modal__inputgroup']} htmlFor='password'>
+          <FiLock size='2rem' />
+          <input
+            className={classes['modal__input']}
+            name='password'
+            type='password'
+            required
+            value={password}
+            placeholder='Password'
+            onChange={handlePasswordChange}
+          />
+        </label>
+        {errorMsg && <div style={{ color: 'red' }}>{errorMsg}</div>}
+      </form>
+      <hr />
+      <div className={classes['modal__bottom']}>
+        <Button onClick={handleCloseModal}>Close</Button>
+        <Button variant='special' type='submit' form='login-form'>
+          Login
+        </Button>
+      </div>
+    </Modal>
+  );
 }
