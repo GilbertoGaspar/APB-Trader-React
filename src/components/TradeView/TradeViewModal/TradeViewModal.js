0a433837fc4b7bd4c81b1a90e91c5ca67dd2a966
Gilberto Gaspar <gilbertogaspar1996@gmail.com>
2021-06-20 09:16:08 -0400

    style: change TradeViewModal to match the new modals

--- src/components/TradeView/TradeViewModal/TradeViewModal.js
+++ src/components/TradeView/TradeViewModal/TradeViewModal.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import * as $ from 'jquery';
+
+import Modal from '../../Modal/Modal';
+import Button from '../../Button/Button';
 
 import classes from './TradeViewModal.module.scss';
 
-const FIREBASE_TRADE_URL = 'https://apb-trader.firebaseio.com/trades.json';
+import { FIREBASE_TRADE_URL } from '../../../firebase/firebase';
 
 export default class TradeViewModal extends Component {
   state = {
     searchTerm: '',
     searchAutoCompleteResults: null,
     validSearchTerm: false,
+    errorMsg: '',
     submitData: {
       weapon: '',
       price: '',
@@ -35,7 +38,29 @@ export default class TradeViewModal extends Component {
     }
   };
 
-  handleSubmit = () => {
+  handleCloseModal = () => {
+    this.setState({
+      searchTerm: '',
+      searchAutoCompleteResults: null,
+      validSearchTerm: false,
+      errorMsg: '',
+      submitData: {
+        weapon: '',
+        price: '',
+        time: '24h',
+        character: '',
+        server: 'Jericho',
+        hudImage: null,
+        unixTimeCreated: null,
+        unixTimeExpires: null,
+        userUid: this.props.user,
+      },
+    });
+    this.props.closeModalCallback();
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
     //Set userUid to current user's.
     this.handleUpdateSubmitData('userUid', this.props.user);
     //Filters data into a list of empty inputs.
@@ -61,16 +86,17 @@ export default class TradeViewModal extends Component {
         this.timeToUnixTime(updatedSubmitData['time']);
       axios
         .post(FIREBASE_TRADE_URL, updatedSubmitData)
-        .then((response) => {
+        .then((res) => {
           alert('Trade submitted!');
-          $('#tradeModalCenter').modal('toggle');
-          $('#btn-refresh').click();
+          this.handleCloseModal();
         })
-        .catch((error) => {
-          console.log(error);
+        .catch(({ message }) => {
+          this.setState({ errorMsg: message });
         });
     } else {
-      alert('Input is not valid!');
+      this.setState({
+        errorMsg: 'Weapon Name invalid, please pick from the autocomplete.',
+      });
     }
   };
 
@@ -138,142 +164,100 @@ export default class TradeViewModal extends Component {
       }
     });
   };
+
   render() {
     return (
-      <div
-        className='modal fade'
-        id='tradeModalCenter'
-        tabIndex='-1'
-        role='dialog'
-        aria-labelledby='tradeModalCenterTitle'
-        aria-hidden='true'
-      >
-        <div className='modal-dialog modal-dialog-centered' role='document'>
-          <div className='modal-content'>
-            <div className='modal-header'>
-              <h5
-                className={`modal-title ${classes['text-black']}`}
-                id='tradeModalLongTitle'
-              >
-                Create Trade
-              </h5>
-              <button
-                type='button'
-                className='close'
-                data-dismiss='modal'
-                aria-label='Close'
-              >
-                <span aria-hidden='true'>&times;</span>
-              </button>
-            </div>
-            <div className={`modal-body ${classes['text-black']}`}>
-              <div className='input-group mb-3'>
-                <div className='input-group-prepend'>
-                  <span className='input-group-text' id='basic-addon1'>
-                    Weapon
-                  </span>
-                </div>
-                <input
-                  type='text'
-                  className='form-control'
-                  placeholder='Weapon Name'
-                  aria-label='Weapon Name'
-                  aria-describedby='basic-addon1'
-                  value={this.state.searchTerm}
-                  onChange={this.handleAutoCompleteChange}
-                />
-              </div>
-              {this.state.searchAutoCompleteResults}
-
-              <div className='input-group mb-3'>
-                <div className='input-group-prepend'>
-                  <span className='input-group-text' id='basic-addon1'>
-                    $
-                  </span>
-                </div>
-                <input
-                  type='number'
-                  className='form-control'
-                  placeholder='Price'
-                  aria-label='Price'
-                  aria-describedby='basic-addon1'
-                  onChange={(event) =>
-                    this.handleUpdateSubmitData('price', event.target.value)
-                  }
-                  value={this.state.submitData.price}
-                />
-              </div>
-              <div className='input-group mb-3'>
-                <div className='input-group-prepend'>
-                  <label
-                    className='input-group-text'
-                    htmlFor='inputGroupSelect01'
-                  >
-                    Time
-                  </label>
-                </div>
-                <select
-                  className='custom-select'
-                  id='inputGroupSelect01'
-                  onChange={(event) =>
-                    this.handleUpdateSubmitData('time', event.target.value)
-                  }
-                >
-                  <option value='24h' defaultValue>
-                    24h
-                  </option>
-                  <option value='48h'>48h</option>
-                </select>
-              </div>
-              <div className='input-group'>
-                <div className='input-group-prepend'>
-                  <span className='input-group-text' id=''>
-                    Character and Server
-                  </span>
-                </div>
-                <input
-                  type='text'
-                  placeholder='Name'
-                  aria-label='Name'
-                  aria-describedby='basic-addon1'
-                  className='form-control'
-                  onChange={(event) =>
-                    this.handleUpdateSubmitData('character', event.target.value)
-                  }
-                />
-                <select
-                  className='custom-select'
-                  id='inputGroupSelect01'
-                  onChange={(event) =>
-                    this.handleUpdateSubmitData('server', event.target.value)
-                  }
-                >
-                  <option value='Jericho' defaultValue>
-                    Jericho
-                  </option>
-                  <option value='Citadel'>Citadel</option>
-                </select>
-              </div>
-            </div>
-            <div className='modal-footer'>
-              <button
-                type='button'
-                className='btn btn-secondary'
-                data-dismiss='modal'
-              >
-                Close
-              </button>
-              <button
-                type='button'
-                className='btn btn-primary'
-                onClick={this.handleSubmit}
-              >
-                Submit
-              </button>
-            </div>
-          </div>
+      <Modal closeModalCallback={this.handleCloseModal}>
+        <h2 className={classes['modal__title']}>Create Trade!</h2>
+        <hr />
+        <form
+          id='weapon-form'
+          className={classes['modal__content']}
+          onSubmit={this.handleSubmit}
+        >
+          <label
+            className={`${classes['modal__inputgroup']} ${classes['modal__inputgroup--autocomplete']}`}
+            htmlFor='weapon'
+          >
+            <input
+              className={classes['modal__input']}
+              name='weapon'
+              required
+              value={this.state.searchTerm}
+              onChange={this.handleAutoCompleteChange}
+              placeholder={`Weapon Name Ex: OCA Nano 'Connoisseur'`}
+            />
+            {this.state.searchAutoCompleteResults}
+          </label>
+          <label className={classes['modal__inputgroup']} htmlFor='price'>
+            <input
+              className={classes['modal__input']}
+              name='price'
+              type='number'
+              required
+              onChange={(event) =>
+                this.handleUpdateSubmitData('price', event.target.value)
+              }
+              value={this.state.submitData.price}
+              placeholder='Price'
+            />
+          </label>
+          <label className={classes['modal__inputgroup']} htmlFor='time'>
+            <select
+              className={`${classes['modal__input']} ${classes['modal__input--select']}`}
+              onChange={(event) =>
+                this.handleUpdateSubmitData('time', event.target.value)
+              }
+              defaultValue=''
+              required
+            >
+              <option value='' disabled hidden>
+                Time Expiration
+              </option>
+              <option value='24h'>24h</option>
+              <option value='48h'>48h</option>
+            </select>
+          </label>
+          <label className={classes['modal__inputgroup']} htmlFor='character'>
+            <input
+              className={classes['modal__input']}
+              name='character'
+              required
+              onChange={(event) =>
+                this.handleUpdateSubmitData('character', event.target.value)
+              }
+              value={this.state.submitData.character}
+              placeholder='Character Name'
+            />
+          </label>
+          <label className={classes['modal__inputgroup']} htmlFor='server'>
+            <select
+              className={`${classes['modal__input']} ${classes['modal__input--select']}`}
+              onChange={(event) =>
+                this.handleUpdateSubmitData('server', event.target.value)
+              }
+              defaultValue=''
+              required
+            >
+              <option value='' disabled hidden>
+                Server Name
+              </option>
+              <option value='Jericho'>Jericho</option>
+              <option value='Citadel'>Citadel</option>
+            </select>
+          </label>
+          {this.state.errorMsg && (
+            <div style={{ color: 'red' }}>{this.state.errorMsg}</div>
+          )}
+        </form>
+        <hr />
+        <div className={classes['modal__bottom']}>
+          <Button onClick={this.handleCloseModal}>Close</Button>
+          <Button variant='special' type='submit' form='weapon-form'>
+            Submit
+          </Button>
         </div>
-      </div>
+      </Modal>
     );
   }
 }
