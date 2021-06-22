ecf11d1cc5ff32851e83bdc4ed313f23f312b4be
Gilberto Gaspar <gilbertogaspar1996@gmail.com>
2021-06-22 08:18:39 -0400

    feat: add new TradeViewCell styles and refactor component

--- src/components/TradeView/TradeViewCell/TradeViewCell.js
+++ src/components/TradeView/TradeViewCell/TradeViewCell.js
@@ -1,26 +1,49 @@
-import React from 'react'
+import React from 'react';
 
-import classes from './TradeViewCell.module.scss'
+import classes from './TradeViewCell.module.scss';
 
 const numberWithCommas = (x) => {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+};
 
-const TradeViewCell = (props) => {
-    return (
-        <div className={classes['trade-view-cell']}>
-            <div className='row'>
-                <img className={classes['trade-view-cell__image']} src={props.hudImage} alt={`${props.weapon}`}></img>
-            </div>
-            <div className={classes['trade-view-cell__row']}>
-                <div className={classes['trade-view-cell__header']}>{props.weapon}</div>
-                <div>Character: {props.character}</div>
-                <div>${numberWithCommas( Math.round(props.price) )}</div>
-                <div className={classes['trade-view-cell__text-top-left']}>{props.server}</div>
-                <div className={classes['trade-view-cell__text-top-right']}>{props.time}</div>
-            </div>
+const TradeViewCell = ({
+  hudImage,
+  weapon,
+  character,
+  price,
+  server,
+  time,
+}) => {
+  return (
+    <div className={classes['cell']}>
+      <div className={classes['cell__upper']}>
+        <div className={classes['cell__upper--left']}>
+          <div className={classes['cell__container--img']}>
+            <img
+              className={classes['cell__img']}
+              alt={`${weapon} symbol`}
+              src={hudImage}
+            />
+          </div>
         </div>
-    )
-}
+        <div className={classes['cell__upper--right']}>
+          <h1 className={classes['cell__title']}>{weapon}</h1>
+          <h2 className={classes['cell__subtitle']}>
+            ${numberWithCommas(Math.round(price))}
+          </h2>
+        </div>
+      </div>
+      <div className={classes['cell__lower']}>
+        <div className={classes['cell__lower--left']}>
+          <div className={classes['cell__box']}>{server}</div>
+          <div className={classes['cell__box']}>{character}</div>
+        </div>
+        <div className={classes['cell__lower--right']}>
+          <div className={classes['cell__box']}>{`Exp: ${time}`}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default TradeViewCell;
